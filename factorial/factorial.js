10bdcf91a296ae5b5d2912494cff2c1307beb0c3
Leandro Bernardo <lbsantos1@stefanini.com>
2023-07-24 17:03:58 -0300

    algorithm exercise / Factorial

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,7 +1,15 @@
 function factorial(num) {
   if (num < 0) {
-    throw new Error("Factorial is not defined for negative numbers")
+    throw new Error('Factorial is not defined for negative numbers')
+  } else if (num === 0 || num === 1) {
+    return 1
+  } else {
+    let res = 1
+    for (let i = 2; i <= num; i++) {
+      res *= i
+    }
+    return res
   }
 }
 
-factorial(-1)
+
